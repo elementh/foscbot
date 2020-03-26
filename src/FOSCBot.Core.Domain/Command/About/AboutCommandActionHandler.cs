50c42ce03e93bd14af9d51395ed23ff339c7d7ef
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-26 22:12:57 +0100

    Implemented AboutCommandAction

---
+++ src/FOSCBot.Core.Domain/Command/About/AboutCommandActionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+using Telegram.Bot.Types.Enums;
+
+namespace FOSCBot.Core.Domain.Command.About
+{
+    public class AboutCommandActionHandler : ActionHandler<AboutCommandAction>
+    {
+        public AboutCommandActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(AboutCommandAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), CoreResources.AboutText, ParseMode.Markdown, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
