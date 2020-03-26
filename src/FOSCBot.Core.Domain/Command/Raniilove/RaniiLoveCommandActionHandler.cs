e959b90486c31985693b55f334b7d2966931bc0f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-26 22:29:30 +0100

    Added RaniiLove

---
+++ src/FOSCBot.Core.Domain/Command/Raniilove/RaniiLoveCommandActionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Raniilove
+{
+    public class RaniiLoveCommandActionHandler : ActionHandler<RaniiloveCommandAction>
+    {
+        public static string RaniiSticker = "CAACAgEAAxkBAAMyXn0ejAABhNQUUOtuxi41w8zpW1kbAAKNAAM4DoIRRihUBMGXYkoYBA";
+
+        public RaniiLoveCommandActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(RaniiloveCommandAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), RaniiSticker, cancellationToken: cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Command/Raniilove/RaniiloveCommandAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Raniilove
+{
+    public class RaniiloveCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command.ToLower() == "/raniilove";
+        }
+    }
+}
