cbe933c86a5bd18e047fd005aae073a87d31c067
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-26 22:24:20 +0100

    Implemented P4Cock command

---
+++ src/FOSCBot.Core.Domain/Command/P4Cock/P4CockCommandAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Command.P4Cock
+{
+    public class P4CockCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command.ToLower() == "/p4cock";
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Command/P4Cock/P4CockCommandActionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Command.P4Cock
+{
+    public class P4CockCommandActionHandler : ActionHandler<P4CockCommandAction>
+    {
+        public static string P4Sticker = "CAACAgQAAxkBAAMvXn0csAE-VH1a5YlL_C3y_uvmyhoAAk4DAAIv22sAAQYHFmm8oYuhGAQ";
+        public P4CockCommandActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(P4CockCommandAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), P4Sticker, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
