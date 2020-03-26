0874b163e6ce8ff553bda9849be571a8c4e03fb0
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-26 22:13:06 +0100

    Implemented StartCommandAction

---
+++ src/FOSCBot.Core.Domain/Command/Start/StartCommandAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Start
+{
+    public class StartCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command.ToLower() == "/start";
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Command/Start/StartCommandActionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+using Telegram.Bot.Types.Enums;
+
+namespace FOSCBot.Core.Domain.Command.Start
+{
+    public class StartCommandActionHandler : ActionHandler<StartCommandAction>
+    {
+        public StartCommandActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(StartCommandAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), CoreResources.StartText, ParseMode.Markdown, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
