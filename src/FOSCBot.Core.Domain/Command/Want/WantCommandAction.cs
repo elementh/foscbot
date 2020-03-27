50b4af2993245b37ab82fe4583a28a8c2f5d8a83
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 10:30:17 +0100

    Implemented WantCommandAction

---
+++ src/FOSCBot.Core.Domain/Command/Want/WantCommandAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Want
+{
+    public class WantCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command.ToLower() == "/want";
+        }
+    }
+}
