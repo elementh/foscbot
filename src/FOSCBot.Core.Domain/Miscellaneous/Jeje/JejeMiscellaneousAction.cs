eddb69a81007501d968d6ee080ebdccb6221cd9b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 09:52:01 +0100

    Implemented JejeMiscellaneousAction

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Jeje/JejeMiscellaneousAction.cs
@@ -0,0 +1,23 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Jeje
+{
+    public class JejeMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            if (RandomProvider.GetThreadRandom().NextDouble() < 0.7d )
+            {
+                return false;
+            }
+
+            return (ctx.Update.Message.Text?.ToLower().Contains("je ") ?? false) ||
+                   (ctx.Update.Message.Text?.ToLower().Contains(" je") ?? false) ||
+                   (ctx.Update.Message.Text?.ToLower().Contains(" je ") ?? false) ||
+                   (ctx.Update.Message.Text?.ToLower().Equals("je") ?? false) ||
+                   (ctx.Update.Message.Text?.ToLower().Contains("je je") ?? false);
+        }
+    }
+}
