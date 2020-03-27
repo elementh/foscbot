73f8b8d52e613012c1c5cc0074ab3a0eb6fac152
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 08:56:49 +0100

    Added Common with RandomProvider

---
+++ src/FOSCBot.Common/Helper/RandomProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace FOSCBot.Common.Helper
+{
+    /// <summary>
+    /// Random provider thanks to https://csharpindepth.com/Articles/Random
+    /// </summary>
+    public static class RandomProvider
+    {    
+        private static int _seed = Environment.TickCount;
+
+        private static readonly ThreadLocal<Random> randomWrapper = new ThreadLocal<Random>
+            (() => new Random(Interlocked.Increment(ref _seed)));
+
+        public static Random GetThreadRandom()
+        {
+            return randomWrapper.Value;
+        }
+    }
+}
