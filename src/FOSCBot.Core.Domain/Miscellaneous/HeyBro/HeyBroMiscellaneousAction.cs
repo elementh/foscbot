5da871f113629adbb7107de03ec60a99a3329a42
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 08:57:43 +0100

    Implemented HeyBroMiscellaneousAction

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/HeyBro/HeyBroMiscellaneousAction.cs
@@ -0,0 +1,15 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.HeyBro
+{
+    public class HeyBroMiscellaneousAction : MessageAction
+    {
+        protected static readonly string StickerId = "DAAKY56MAAQspeiAK-9YiGAQ";
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return RandomProvider.GetThreadRandom().NextDouble() > 0.6d && (ctx.Update.Message.Sticker?.FileId.EndsWith(StickerId) ?? false);
+        }
+    }
+}
