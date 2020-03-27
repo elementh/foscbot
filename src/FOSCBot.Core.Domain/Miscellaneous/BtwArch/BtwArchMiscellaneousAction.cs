b68b3475ee9474a591460a06070e2cfddc3d5517
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 11:37:42 +0100

    Implemented Btw I use Arch

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/BtwArch/BtwArchMiscellaneousAction.cs
@@ -0,0 +1,15 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.BtwArch
+{
+    public class BtwArchMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return RandomProvider.GetThreadRandom().NextDouble() > 0.7d && (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("arch") ?? false);
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/BtwArch/BtwArchMiscellaneousActionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+using Telegram.Bot.Types.Enums;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.BtwArch
+{
+    public class BtwArchMiscellaneousActionHandler : ActionHandler<BtwArchMiscellaneousAction>
+    {
+        public BtwArchMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(BtwArchMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), "`Btw I run on Arch Linux.`", ParseMode.Markdown,
+                replyToMessageId: request.MessageId, cancellationToken: cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
