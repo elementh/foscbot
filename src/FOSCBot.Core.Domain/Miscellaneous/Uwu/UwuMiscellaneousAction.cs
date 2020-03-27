f3bb6ae7b4d71b9ec872e953cb58c75db7ad5de6
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 10:19:50 +0100

    Implemented UwuMiscellaneousAction

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Uwu/UwuMiscellaneousAction.cs
@@ -0,0 +1,14 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Uwu
+{
+    public class UwuMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return RandomProvider.GetThreadRandom().NextDouble() < 0.3d && (ctx.Update.Message.Text?.ToLower().Contains("uwu") ?? false);
+        }
+    }
+}
