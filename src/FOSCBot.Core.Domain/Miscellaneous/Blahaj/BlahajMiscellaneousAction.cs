8e95e84c00c0f2f5b682461d19f900d36b52903e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 12:37:57 +0100

    Implemented Blahaj

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Blahaj/BlahajMiscellaneousAction.cs
@@ -0,0 +1,14 @@
+﻿using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Blahaj
+{
+    public class BlahajMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("blahaj") ?? false;
+        }
+    }
+}
