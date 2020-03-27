d0aa5d7cb99382dedf657445a55071807856d5ac
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 10:55:04 +0100

    Implemented NginxMiscAction

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Nginx/NginxMiscellaneousAction.cs
@@ -0,0 +1,14 @@
+﻿using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Nginx
+{
+    public class NginxMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("nginx") ?? false;
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Nginx/NginxMiscellaneousActionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+using Telegram.Bot.Types;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Nginx
+{
+    public class NginxMiscellaneousActionHandler : ActionHandler<NginxMiscellaneousAction>
+    {
+        public NginxMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(NginxMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            var bytes = Convert.FromBase64String(CoreResources.NginxImage);
+            await using (var stream = await new StreamContent(new MemoryStream(bytes)).ReadAsStreamAsync())
+            {
+                await Ctx.Client.SendPhotoAsync(Ctx.GetTelegramChat(), new InputMedia(stream, "heybroniced.jpg"), 
+                    cancellationToken: cancellationToken);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
