199e7c8e068aef53693f97801c7fa4484e8d2a5f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-27 11:13:06 +0100

    Implemented SadMiscellaneousAction

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Sad/SadMiscellaneousAction.cs
@@ -0,0 +1,15 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Sad
+{
+    public class SadMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return RandomProvider.GetThreadRandom().NextDouble() > 0.6d && (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("sad") ?? false);
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Sad/SadMiscellaneousActionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Sad
+{
+    public class SadMiscellaneousActionHandler : ActionHandler<SadMiscellaneousAction>
+    {
+        public SadMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(SadMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), SadCrstian, cancellationToken: cancellationToken);
+
+            return Unit.Value;
+        }
+        
+        public static readonly string SadCrstian = "CAACAgQAAxkBAAI5DF59uqkJYnqzc5LcnEC_bdp0rerIAAJsAwACmOejAAG_qYNUT_L_exgE";
+    }
+}
