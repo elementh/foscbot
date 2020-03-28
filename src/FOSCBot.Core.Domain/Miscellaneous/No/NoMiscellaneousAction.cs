5a0d5b836e67d414c684635a6c50a8a47530aae7
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-28 18:32:48 +0100

    More No actions

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/No/NoMiscellaneousAction.cs
@@ -0,0 +1,20 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.No
+{
+    public class NoMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            if (RandomProvider.GetThreadRandom().NextDouble() < 0.5d )
+            {
+                return false;
+            }
+
+            return (ctx.Update.Message.Text?.Equals("NO") ?? false) ||
+                   (ctx.Update.Message.Text?.Equals("NOPE") ?? false);
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/No/NoMiscellaneousActionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.No
+{
+    public class NoMiscellaneousActionHandler : ActionHandler<NoMiscellaneousAction>
+    {
+        public NoMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(NoMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendVideoAsync(Ctx.GetTelegramChat(), VideoLinks.Nope, cancellationToken: cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
