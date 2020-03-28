1a7da0f9141b3cb2817dfc2c3099d8f60d6ac891
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-28 17:35:02 +0100

    Added ICE action

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Ice/IceMiscellaneousAction.cs
@@ -0,0 +1,29 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Ice
+{
+    public class IceMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            if (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("fucking ice") ?? false)
+            {
+                return true;
+            }
+            
+            if (RandomProvider.GetThreadRandom().NextDouble() < 0.2d )
+            {
+                return false;
+            }
+
+            return (ctx.Update.Message.Text?.ToLower().StartsWith("ice") ?? false)
+                   || (ctx.Update.Message.Text?.ToLower().Contains(" ice ") ?? false)
+                   || (ctx.Update.Message.Text?.ToLower().Contains(" ice?") ?? false)
+                   || (ctx.Update.Message.Text?.ToLower().Contains(" hielo ") ?? false)
+                   || ctx.GetMessageOrDefault()?.Sticker?.Emoji == "🥶";
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Ice/IceMiscellaneousActionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Ice
+{
+    public class IceMiscellaneousActionHandler : ActionHandler<IceMiscellaneousAction>
+    {
+        public IceMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(IceMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendVideoAsync(Ctx.GetTelegramChat(), VideoLinks.Ice, cancellationToken: cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
