d2b301ad974591fd2f5716a72c69efb05100a1cb
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-28 17:34:56 +0100

    Added Nice action

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Nice/NiceMiscellaneousAction.cs
@@ -0,0 +1,14 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Nice
+{
+    public class NiceMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return RandomProvider.GetThreadRandom().NextDouble() < 0.7d && (ctx.Update.Message.Text?.Contains("NICE") ?? false);
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Nice/NiceMiscellaneousActionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Nice
+{
+    public class NiceMiscellaneousActionHandler : ActionHandler<NiceMiscellaneousAction>
+    {
+        public NiceMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(NiceMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendVideoAsync(Ctx.GetTelegramChat(), VideoLinks.Nice, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
