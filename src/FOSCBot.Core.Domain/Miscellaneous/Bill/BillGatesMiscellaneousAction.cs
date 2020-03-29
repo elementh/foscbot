8079fdb46a247c26fa0153bba30603b15f36bf6b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-29 14:24:45 +0200

    Added Bill old friend

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Bill/BillGatesMiscellaneousAction.cs
@@ -0,0 +1,14 @@
+﻿using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Bill
+{
+    public class BillGatesMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("bill gates") ?? false;
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Bill/BillGatesMiscellaneousActionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Common.Helper;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Bill
+{
+    public class BillGatesMiscellaneousActionHandler : ActionHandler<BillGatesMiscellaneousAction>
+    {
+        
+        public BillGatesMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+        
+        public override async Task<Unit> Handle(BillGatesMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            var randomSticker = Stickers[RandomProvider.GetThreadRandom().Next(0, Stickers.Length)];
+
+            await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), randomSticker, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+        
+        public static readonly string[] Stickers = {
+            "CAACAgQAAxkBAAI7GV6AjQuBNOLeYNKM1SBiRWB7tnuiAAIVDgAC_wRTAAGOgxwVhlJSlBgE",
+            "CAACAgIAAxkBAAI7G16AkmXDeUy_ub_bezQ1rCEoQJhaAAKEBwACYyviCdnd2EStFD0DGAQ",
+            "CAACAgIAAxkBAAI7HV6AkohZo2o9brFBO2wXVaZw7WusAAKRBwACYyviCcQ168JPLJQxGAQ",
+            "CAACAgIAAxkBAAI7H16AkqTrAQ7RuRodfkDnR22FgHi5AAKXBwACYyviCdpCtHXzTycRGAQ",
+            "CAACAgIAAxkBAAI7IV6AkqefjL2tvmJFtmVg04eF3YLsAAKPBwACYyviCR9FAgcEOrD5GAQ",
+            "CAACAgIAAxkBAAI7I16Akqs0MGoJKRvozewtn8rD-s-BAAKYBwACYyviCTG5W40KakR2GAQ",
+            "CAACAgIAAxkBAAI7Jl6Akxqjt-lLYrVUo5I4NjgNaNigAAKDBwACYyviCX-4JBrvWaA3GAQ"
+        };
+    }
+}
