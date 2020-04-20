34a80e8820b5833b6cea50eee24ba5d19cd2214f
LinuxCT <3966313+linuxct@users.noreply.github.com>
2020-04-20 23:25:15 +0200

    Update UpctMiscellaneousActionHandler.cs

--- src/FOSCBot.Core.Domain/Miscellaneous/Upct/UpctMiscellaneousActionHandler.cs
+++ src/FOSCBot.Core.Domain/Miscellaneous/Upct/UpctMiscellaneousActionHandler.cs
@@ -15,12 +15,14 @@ namespace FOSCBot.Core.Domain.Miscellaneous.Upct
 
         public override async Task<Unit> Handle(UpctMiscellaneousAction request, CancellationToken cancellationToken)
         {
-			var stickerString = RandomProvider.GetThreadRandom().NextDouble() > 0.2d ? "CAACAgQAAxkBAAJNW16eEHOauvBkLuaD-jL95s86vn2qAAJuAwACmOejAAEys6bCdTOD7RgE" : "CAACAgQAAxkBAAJNXV6eEJLQHwl-8el7YOYYJUF9l8ymAAJZAgACkNStBjfoiv3ywvd8GAQ";
-			await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), stickerString, cancellationToken: cancellationToken);
-			
-			if (RandomProvider.GetThreadRandom().NextDouble() > 0.1d) 
-				await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), "cAmPuS dE eXcElEnCiA iNtErNaCiOnAl", cancellationToken: cancellationToken);
-            
+            var stickerString = RandomProvider.GetThreadRandom().NextDouble() > 0.2d
+                ? "CAACAgQAAxkBAAJNW16eEHOauvBkLuaD-jL95s86vn2qAAJuAwACmOejAAEys6bCdTOD7RgE"
+                : "CAACAgQAAxkBAAJNXV6eEJLQHwl-8el7YOYYJUF9l8ymAAJZAgACkNStBjfoiv3ywvd8GAQ";
+            await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), stickerString, cancellationToken: cancellationToken);
+
+            if (RandomProvider.GetThreadRandom().NextDouble() > 0.1d)
+                await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), "cAmPuS dE eXcElEnCiA iNtErNaCiOnAl", cancellationToken: cancellationToken);
+
             return Unit.Value;
         }
     }
