e3449ef56aaf659d1a4e412712a2c96b080297e4
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-22 19:53:47 +0200

    Added Bacon and Metaphor client interfaces

---
+++ src/FOSCBot.Infrastructure.Contract/Client/IBaconClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FOSCBot.Infrastructure.Contract.Client
+{
+    public interface IBaconClient
+    {
+        Task<string> Get(string type, int sentences, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/FOSCBot.Infrastructure.Contract/Client/IMetaphorClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace FOSCBot.Infrastructure.Contract.Client
+{
+    public interface IMetaphorClient
+    {
+        Task<string> GetSentence(int quantity, CancellationToken cancellationToken = default);
+        Task<string> GetParagraph(int quantity, CancellationToken cancellationToken = default);
+    }
+}
