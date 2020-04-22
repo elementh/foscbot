d6cc307d1a7ddde35aed8fdfb4c49b279cc94775
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-22 22:14:34 +0200

    Added FallbackAction with 5% chance

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Fallback/FallbackAction.cs
@@ -0,0 +1,16 @@
+﻿using FOSCBot.Common.Helper;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Fallback
+{
+    public class FallbackAction : MessageAction
+    {
+        public new int Order = 1100;
+        
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return RandomProvider.GetThreadRandom().Next(0, 100) > 95;
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Fallback/FallbackActionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Common.Helper;
+using FOSCBot.Infrastructure.Contract.Service;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+using Telegram.Bot.Types.Enums;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Fallback
+{
+    public class FallbackActionHandler : ActionHandler<FallbackAction>
+    {
+        protected ILipsumService LipsumService;
+        
+        public FallbackActionHandler(INavigatorContext ctx, ILipsumService lipsumService) : base(ctx)
+        {
+            LipsumService = lipsumService;
+        }
+
+        public override async Task<Unit> Handle(FallbackAction request, CancellationToken cancellationToken)
+        {
+            string sentence;
+            
+            if (RandomProvider.GetThreadRandom().Next(0, 10) > 5)
+            {
+                sentence = await LipsumService.GetBacon(cancellationToken: cancellationToken);
+            }
+            else
+            {
+                sentence = await LipsumService.GetMetaphorSentence(cancellationToken: cancellationToken);
+            }
+            
+            await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), sentence, ParseMode.Markdown,
+                replyToMessageId: request.MessageId, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
