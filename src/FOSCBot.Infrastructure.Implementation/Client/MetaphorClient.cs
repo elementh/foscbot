40aeaa77a9197d0b8c27900ed151a8bf7645d4de
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-22 19:54:45 +0200

    Implemented MetaphorClient

---
+++ src/FOSCBot.Infrastructure.Implementation/Client/MetaphorClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Client;
+using Microsoft.Extensions.Options;
+
+namespace FOSCBot.Infrastructure.Implementation.Client
+{
+    public class MetaphorClient : IMetaphorClient
+    {
+        protected readonly HttpClient Client;
+        protected readonly MetaphorClientOptions Options;
+
+        public MetaphorClient(HttpClient client, IOptions<MetaphorClientOptions> options)
+        {
+            Client = client;
+            Options = options.Value;
+
+            Client.BaseAddress = new Uri(Options.ApiUrl);
+        }
+
+        public async Task<string> GetSentence(int quantity, CancellationToken cancellationToken = default)
+        {
+            var response = await Client.GetAsync($"sentences/{quantity}", cancellationToken);
+            
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetParagraph(int quantity, CancellationToken cancellationToken = default)
+        {
+            var response = await Client.GetAsync($"paragraphs/{quantity}", cancellationToken);
+            
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+        
+        
+        public class MetaphorClientOptions
+        {
+            public string ApiUrl { get; set; }
+        }
+    }
+}
