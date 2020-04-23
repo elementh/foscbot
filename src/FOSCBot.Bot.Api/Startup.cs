c07b992d7c34f94e556d31c487609f22a161f546
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-23 19:20:26 +0200

    Removed unused directives in startup

--- src/FOSCBot.Bot.Api/Startup.cs
+++ src/FOSCBot.Bot.Api/Startup.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FOSCBot.Core.Domain.Inline;
 using FOSCBot.Core.Domain.Inline.Default;
 using FOSCBot.Infrastructure.Contract.Client;
 using FOSCBot.Infrastructure.Contract.Service;
@@ -11,12 +7,9 @@ using FOSCBot.Infrastructure.Implementation.Service;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Navigator;
 using Polly;
 
