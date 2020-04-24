1c1ab9f991d94f1b8230534ed32768cc61e8231f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 09:52:31 +0200

    Added Inspiro Client and Service interfaces

---
+++ src/FOSCBot.Infrastructure.Contract/Client/IInspiroClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace FOSCBot.Infrastructure.Contract.Client
+{
+    public interface IInspiroClient
+    {
+        Task<string> Get(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/FOSCBot.Infrastructure.Contract/Service/IInspiroService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace FOSCBot.Infrastructure.Contract.Service
+{
+    public interface IInspiroService
+    {
+        Task<string> GetInspiroImage(CancellationToken cancellationToken = default);
+    }
+}
