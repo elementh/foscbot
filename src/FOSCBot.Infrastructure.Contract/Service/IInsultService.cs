b34fb5f17967bb9cb407ba494acdfbe776495e61
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 10:43:29 +0200

    Added Insult interfaces

---
+++ src/FOSCBot.Infrastructure.Contract/Service/IInsultService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace FOSCBot.Infrastructure.Contract.Service
+{
+    public interface IInsultService
+    {
+        Task<string> GetInsult(CancellationToken cancellationToken = default);
+    }
+}
