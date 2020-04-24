fae1e72acb904b2848832c7a04fdaa586cc694de
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 11:51:35 +0200

    Added matalo command action

---
+++ src/FOSCBot.Core.Domain/Command/Matalo/MataloCommandAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Matalo
+{
+    public class MataloCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command.ToLower() == "/matalo";
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Command/Matalo/MataloCommandActionHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Service;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Matalo
+{
+    public class MataloCommandActionHandler : ActionHandler<MataloCommandAction>
+    {
+        private readonly IInsultService _insultService;
+        private readonly IYesNoService _yesNoService;
+
+        public MataloCommandActionHandler(INavigatorContext ctx, IInsultService insultService, IYesNoService yesNoService) : base(ctx)
+        {
+            _insultService = insultService;
+            _yesNoService = yesNoService;
+        }
+
+        public override async Task<Unit> Handle(MataloCommandAction request, CancellationToken cancellationToken)
+        {
+            if (request.ReplyToMessageId.HasValue)
+            {
+                var insult = await _insultService.GetInsult(cancellationToken);
+
+                if (!string.IsNullOrWhiteSpace(insult))
+                {
+                    await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), insult, replyToMessageId: request.ReplyToMessageId.Value,
+                        cancellationToken: cancellationToken);
+                }
+            }
+            else
+            {
+                var answer = await _yesNoService.GetNoImage(cancellationToken);
+
+                if (!string.IsNullOrWhiteSpace(answer))
+                {
+                    await Ctx.Client.SendVideoAsync(Ctx.GetTelegramChat(), answer, replyToMessageId: request.MessageId,
+                        cancellationToken: cancellationToken);
+                }
+            }
+
+            return Unit.Value;
+        }
+    }
+}
