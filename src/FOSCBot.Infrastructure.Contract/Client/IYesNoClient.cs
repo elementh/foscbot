5cf0e6ac69e42e882d1b28f902719665132628f0
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 11:50:41 +0200

    Added YesNoClient interface and answer model

---
+++ src/FOSCBot.Infrastructure.Contract/Client/IYesNoClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Model;
+
+namespace FOSCBot.Infrastructure.Contract.Client
+{
+    public interface IYesNoClient
+    {
+        Task<YesNoAnswerModel> GetAnswer(string type, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/FOSCBot.Infrastructure.Contract/Model/YesNoAnswerModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FOSCBot.Infrastructure.Contract.Model
+{
+    public class YesNoAnswerModel
+    {
+        [JsonPropertyName("answer")]
+        public string Answer { get; set; }
+        [JsonPropertyName("forced")]
+        public bool Forced { get; set; }
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+    }
+}
