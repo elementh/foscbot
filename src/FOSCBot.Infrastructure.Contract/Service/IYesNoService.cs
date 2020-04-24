628a6088110478b25842a92bcb4651d2dacfbf3a
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 11:51:11 +0200

    Created and implemented YesNoService

---
+++ src/FOSCBot.Infrastructure.Contract/Service/IYesNoService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace FOSCBot.Infrastructure.Contract.Service
+{
+    public interface IYesNoService
+    {
+        Task<string> GetYesImage(CancellationToken cancellationToken);
+        Task<string> GetNoImage(CancellationToken cancellationToken);
+        Task<string> GetMaybeImage(CancellationToken cancellationToken);
+    }
+}
