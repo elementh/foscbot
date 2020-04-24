5717d0cb0475dd9e4670020380c37fbd9edd6264
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 11:50:53 +0200

    Implemented YesNoClient

---
+++ src/FOSCBot.Infrastructure.Implementation/Client/YesNoClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Client;
+using FOSCBot.Infrastructure.Contract.Model;
+using Microsoft.Extensions.Options;
+
+namespace FOSCBot.Infrastructure.Implementation.Client
+{
+    public class YesNoClient : IYesNoClient
+    {
+        protected readonly HttpClient Client;
+        protected readonly YesNoClientOptions Options;
+
+        public YesNoClient(HttpClient client, IOptions<YesNoClientOptions> options)
+        {
+            Client = client;
+            Options = options.Value;
+
+            Client.BaseAddress = new Uri(Options.ApiUrl);
+        }
+
+        public async Task<YesNoAnswerModel> GetAnswer(string type, CancellationToken cancellationToken = default)
+        {
+            var response = await Client.GetAsync($"api?force={type}", cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+            
+            var answer = await JsonSerializer.DeserializeAsync<YesNoAnswerModel>(await response.Content.ReadAsStreamAsync(),
+                cancellationToken: cancellationToken);
+
+            return answer;
+        }
+
+        public class YesNoClientOptions
+        {
+            public string ApiUrl { get; set; }
+        }
+    }
+}
