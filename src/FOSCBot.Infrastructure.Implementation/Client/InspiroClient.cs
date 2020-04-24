2ad9fe95fb18914c903b6aaa7755faeae317fd97
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 09:52:51 +0200

    Implemented InspiroClient and InspiroService

---
+++ src/FOSCBot.Infrastructure.Implementation/Client/InspiroClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Client;
+using Microsoft.Extensions.Options;
+
+namespace FOSCBot.Infrastructure.Implementation.Client
+{
+    public class InspiroClient : IInspiroClient
+    {
+        protected readonly HttpClient Client;
+        protected readonly InspiroClientOptions Options;
+
+        public InspiroClient(HttpClient client, IOptions<InspiroClientOptions> options)
+        {
+            Client = client;
+            Options = options.Value;
+
+            Client.BaseAddress = new Uri(Options.ApiUrl);
+        }
+
+        public async Task<string> Get(CancellationToken cancellationToken = default)
+        {
+            var response = await Client.GetAsync("api?generate=true", cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public class InspiroClientOptions
+        {
+            public string ApiUrl { get; set; }
+        }
+    }
+}
