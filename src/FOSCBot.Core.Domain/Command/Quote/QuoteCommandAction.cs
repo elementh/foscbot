394f6c3e31e64ba2194cf4c8d6bcc96f50e66902
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 09:53:20 +0200

    Implemented quote command

---
+++ src/FOSCBot.Core.Domain/Command/Quote/QuoteCommandAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Quote
+{
+    public class QuoteCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command.ToLower() == "/quote";
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Command/Quote/QuoteCommandActionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Service;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Quote
+{
+    public class QuoteCommandActionHandler : ActionHandler<QuoteCommandAction>
+    {
+        private readonly IInspiroService _inspiroService;
+
+        public QuoteCommandActionHandler(INavigatorContext ctx, IInspiroService inspiroService) : base(ctx)
+        {
+            _inspiroService = inspiroService;
+        }
+
+        public override async Task<Unit> Handle(QuoteCommandAction request, CancellationToken cancellationToken)
+        {
+            var image = await _inspiroService.GetInspiroImage(cancellationToken);
+
+            await Ctx.Client.SendPhotoAsync(Ctx.GetTelegramChat(), image, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
