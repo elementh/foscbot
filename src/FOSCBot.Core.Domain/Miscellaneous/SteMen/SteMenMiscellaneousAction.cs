3e1c2d272446c77b3bae76f909bae77881f13ae2
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 15:56:15 +0200

    Added stemen action

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/SteMen/SteMenMiscellaneousAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+namespace FOSCBot.Core.Domain.Miscellaneous.SteMen
+{
+    public class SteMenMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("ste men") ?? false;
+        }
+    }
+}
