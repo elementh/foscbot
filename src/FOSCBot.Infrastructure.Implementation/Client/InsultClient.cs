8cb8fe90046eaa530389d2bdf2858357027da2a1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-04-24 10:51:30 +0200

    Implemented insult services

---
+++ src/FOSCBot.Infrastructure.Implementation/Client/InsultClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Client;
+using Microsoft.Extensions.Options;
+
+namespace FOSCBot.Infrastructure.Implementation.Client
+{
+    public class InsultClient : IInsultClient
+    {
+        protected readonly HttpClient Client;
+        protected readonly InsultClientOptions Options;
+
+        public InsultClient(HttpClient client, IOptions<InsultClientOptions> options)
+        {
+            Client = client;
+            Options = options.Value;
+
+            Client.BaseAddress = new Uri(Options.ApiUrl);
+        }
+        public async Task<string> Get(CancellationToken cancellationToken = default)
+        {
+            var response = await Client.GetAsync("generate_insult.php?lang=en", cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public class InsultClientOptions
+        {
+            public string ApiUrl { get; set; }
+        }
+    }
+}
