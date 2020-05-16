a52d5bfa8e17bc726875aac756a858f1919f1791
LinuxCT <3966313+linuxct@users.noreply.github.com>
2020-05-17 00:34:51 +0200

    Added Succ

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Succ/SuccMiscellaneousAction.cs
@@ -0,0 +1,13 @@
+﻿using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+namespace FOSCBot.Core.Domain.Miscellaneous.Succ
+{
+    public class SuccMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return (ctx.GetMessageOrDefault()?.Text?.Equals("SUCC") ?? false) || (ctx.GetMessageOrDefault()?.Text?.Equals("SAC") ?? false);
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Succ/SuccMiscellaneousActionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Common.Helper;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Succ
+{
+    public class SuccMiscellaneousActionHandler : ActionHandler<SuccMiscellaneousAction>
+    {
+        public SuccMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(SuccMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            if (RandomProvider.GetThreadRandom().NextDouble() < 0.8d)
+                await Ctx.Client.SendVideoAsync(Ctx.GetTelegramChat(), CoreLinks.Succ, cancellationToken: cancellationToken);
+            else
+                await Ctx.Client.SendVideoAsync(Ctx.GetTelegramChat(), CoreLinks.SuccWithTeeth, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
