6834878e55998dd5438c39cc54ce54b0a1206949
linuxct <3966313+linuxct@users.noreply.github.com>
2020-06-17 01:11:34 +0200

    Add Torture action
    Blub blub blub blub blub blub blub blub blub blub blub blub

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Torture/TortureMiscellaneousAction.cs
@@ -0,0 +1,17 @@
+using FOSCBot.Common.Helper;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Torture
+{
+    public class TortureMiscellaneousAction : MessageAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("cock and balls torture") ?? false) 
+            || (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("cock and ball torture") ?? false) 
+            || (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("cum blast me") ?? false);
+        }
+    }
+}
