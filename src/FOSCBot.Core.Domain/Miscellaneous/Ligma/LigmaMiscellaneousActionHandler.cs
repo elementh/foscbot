653a7c56c12b8896b9dc349f7cdc3c561bcd58e1
linuxct <3966313+linuxct@users.noreply.github.com>
2020-06-17 18:44:19 +0200

    Implement Ligma
    It's so sad this PR died of ligma

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Ligma/LigmaMiscellaneousActionHandler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Common.Helper;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator;
+using Navigator.Abstraction;
+using Navigator.Actions;
+using Telegram.Bot.Types;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Ligma
+{
+    public class LigmaMiscellaneousActionHandler : ActionHandler<LigmaMiscellaneousAction>
+    {
+        private readonly string LIGMA_TEXT = "ligma balls";
+        public LigmaMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(LigmaMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            if (RandomProvider.GetThreadRandom().NextDouble() >= 0.75)
+            {
+                var bytes = Convert.FromBase64String(CoreResources.LigmaHardAudio);
+                await using var stream = await new StreamContent(new MemoryStream(bytes)).ReadAsStreamAsync();
+                
+                await Ctx.Client.SendVoiceAsync(Ctx.GetTelegramChat(), new InputMedia(stream, LIGMA_TEXT.ToUpper()), duration: 4, cancellationToken: cancellationToken);
+            }
+            else
+            {
+                var bytes = Convert.FromBase64String(CoreResources.LigmaSoftAudio);
+                await using var stream = await new StreamContent(new MemoryStream(bytes)).ReadAsStreamAsync();
+                
+                await Ctx.Client.SendVoiceAsync(Ctx.GetTelegramChat(), new InputMedia(stream, LIGMA_TEXT), duration: 3, cancellationToken: cancellationToken);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
