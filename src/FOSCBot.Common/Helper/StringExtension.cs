3272dcd7205c6dc17ede2b5529f47b53de67b36d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-01-18 16:49:01 +0100

    Added string extension method

---
+++ src/FOSCBot.Common/Helper/StringExtension.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FOSCBot.Common.Helper
+{
+    public static class StringExtension
+    {
+        public static bool ContainsWord(this string str, string word)
+        {
+            var lastIndex = 0;
+            
+            foreach (var character in word)
+            {
+                if (str.IndexOf(character, lastIndex) == -1)
+                {
+                    return false;
+                }
+                
+                lastIndex = str.IndexOf(character, lastIndex);
+            }
+
+            return true;
+        }
+        
+        public static IEnumerable<int> GetIndexListFor(this string str, string word)
+        {
+            var indexList = new List<int>();
+            
+            var lastIndex = 0;
+            
+            foreach (var character in word)
+            {
+                if (str.IndexOf(character, lastIndex) == -1)
+                {
+                    return new int[] {};
+                }
+                
+                lastIndex = str.IndexOf(character, lastIndex);
+                
+                indexList.Add(lastIndex);
+            }
+
+            return indexList;
+        }
+        
+        public static string ReplaceAt(this string input, int index, char newChar)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            char[] chars = input.ToCharArray();
+            chars[index] = newChar;
+            return new string(chars);
+        }
+        
+        public static string ReplaceAt(this string input, int index, string newChars)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            var chars = input.ToCharArray().ToList();
+            
+            chars.InsertRange(index, newChars);
+            //
+            // chars.RemoveAt(index);
+            
+            return new string(chars.ToString());
+        }
+    }
+}
