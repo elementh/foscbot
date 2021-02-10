83d7bf42e1b1c60677083523638b78bae3db7dca
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-10 18:53:33 +0100

    Added bottomify command

---
+++ src/FOSCBot.Core.Domain/Command/Bottomify/BottomifyCommandAction.cs
@@ -0,0 +1,14 @@
+﻿using Navigator.Abstractions;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Command.Bottomify
+{
+    public class BottomifyCommandAction : CommandAction
+    {
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            return Command.ToLower() == "/bottomify";
+
+        }
+    }
+}
