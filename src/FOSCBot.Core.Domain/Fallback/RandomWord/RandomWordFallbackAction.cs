984f3a4011eefa4de0981f1e51bd374951f81b1d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-24 12:10:21 +0100

    Implemented RandomWordFallbackAction

---
+++ src/FOSCBot.Core.Domain/Fallback/RandomWord/RandomWordFallbackAction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using FOSCBot.Common.Helper;
+using Microsoft.Extensions.Caching.Memory;
+using Navigator.Abstractions;
+using Navigator.Abstractions.Extensions;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Fallback.RandomWord
+{
+    public class RandomWordFallbackAction : MessageAction
+    {
+        public new int Order;
+
+        private readonly IMemoryCache _memoryCache;
+        
+        public string Word { get; protected set; }
+
+        public RandomWordFallbackAction(IMemoryCache memoryCache)
+        {
+            Order = 1050;
+            _memoryCache = memoryCache;
+        }
+
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            if (_memoryCache.TryGetValue($"_{nameof(RandomWordFallbackAction)}_{ctx.GetTelegramChat().Id}", out _))
+            {
+                return false;
+            }
+
+            var words = ctx.GetMessageOrDefault()?.Text.Trim().Split(" ");
+            
+            if (words?.Length == 1)
+            {
+                Word = words.FirstOrDefault() ?? string.Empty;
+
+                if (string.IsNullOrWhiteSpace(Word) || !Word.IsAllUpper() || Word.Length < 4)
+                {
+                    return false;
+                }
+                
+                return _memoryCache.Set($"_{nameof(RandomWordFallbackAction)}_{ctx.GetTelegramChat().Id}", true, TimeSpan.FromMinutes(1));
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Fallback/RandomWord/RandomWordFallbackActionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Infrastructure.Contract.Service;
+using MediatR;
+using Navigator.Abstractions;
+using Navigator.Abstractions.Extensions;
+using Navigator.Extensions.Actions;
+using Telegram.Bot.Types.InputFiles;
+
+namespace FOSCBot.Core.Domain.Fallback.RandomWord
+{
+    public class RandomWordFallbackActionHandler : ActionHandler<RandomWordFallbackAction>
+    {
+        private readonly IGiphyService _giphyService;
+        public RandomWordFallbackActionHandler(INavigatorContext ctx, IGiphyService giphyService) : base(ctx)
+        {
+            _giphyService = giphyService;
+        }
+
+        public override async Task<Unit> Handle(RandomWordFallbackAction request, CancellationToken cancellationToken)
+        {
+            var gifUrl = await _giphyService.Get(request.Word, cancellationToken);
+
+            if (gifUrl is not null)
+            {
+                await Ctx.Client.SendAnimationAsync(Ctx.GetTelegramChat(), new InputOnlineFile(gifUrl), cancellationToken: cancellationToken);
+            }
+            
+            return Unit.Value;
+        }
+    }
+}
