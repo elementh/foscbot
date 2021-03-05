dc06974488d067c0dde5a7adc24357864e60feb4
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-05 13:29:54 +0100

    Added Wagh action Thanks https://twitter.com/quorth for the regex.

---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Wagh/WaghMiscellaneousAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Navigator.Abstractions;
+using Navigator.Abstractions.Extensions;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Wagh
+{
+    public class WaghMiscellaneousAction : MessageAction
+    {
+        private static readonly string _pattern = @"^WA*GH$";
+        public override bool CanHandle(INavigatorContext ctx)
+        {
+            var regex = new Regex(_pattern);
+
+            return regex.IsMatch(ctx.GetMessageOrDefault()?.Text ?? string.Empty);
+        }
+    }
+}
---
+++ src/FOSCBot.Core.Domain/Miscellaneous/Wagh/WaghMiscellaneousActionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FOSCBot.Core.Domain.Resources;
+using MediatR;
+using Navigator.Abstractions;
+using Navigator.Abstractions.Extensions;
+using Navigator.Extensions.Actions;
+
+namespace FOSCBot.Core.Domain.Miscellaneous.Wagh
+{
+    public class WaghMiscellaneousActionHandler : ActionHandler<WaghMiscellaneousAction>
+    {
+        public WaghMiscellaneousActionHandler(INavigatorContext ctx) : base(ctx)
+        {
+        }
+
+        public override async Task<Unit> Handle(WaghMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            await Ctx.Client.SendVideoAsync(Ctx.GetTelegramChat(), CoreLinks.Orks, cancellationToken: cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
