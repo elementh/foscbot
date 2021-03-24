680a048cabeef44ab1cd2bff5dfe2745f51b20bb
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-24 13:00:58 +0100

    Added cooldown to ipad action

--- src/FOSCBot.Core.Domain/Miscellaneous/Ipad/IpadMiscellaneousAction.cs
+++ src/FOSCBot.Core.Domain/Miscellaneous/Ipad/IpadMiscellaneousAction.cs
@@ -1,13 +1,42 @@
-﻿using Navigator.Abstractions;
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+using Navigator.Abstractions;
+using Navigator.Abstractions.Extensions;
 using Navigator.Extensions.Actions;
 
 namespace FOSCBot.Core.Domain.Miscellaneous.Ipad
 {
     public class IpadMiscellaneousAction : MessageAction
     {
+        private readonly IMemoryCache _memoryCache;
+
+        public IpadMiscellaneousAction(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
         public override bool CanHandle(INavigatorContext ctx)
         {
-            return ctx.Update.Message.Text?.ToLower().Contains(" ipad") ?? false;
+            if (_memoryCache.TryGetValue($"_{nameof(IpadMiscellaneousAction)}_{ctx.GetTelegramChatOrDefault()?.Id}", out _))
+            {
+                return false;
+            }
+
+            if (ctx.Update.Message.Text?.ToLower().Contains(" ipad") != null)
+            {
+                try
+                {
+                    _memoryCache.Set($"_{nameof(IpadMiscellaneousAction)}_{ctx.GetTelegramChatOrDefault()?.Id}", 1, TimeSpan.FromMinutes(15));
+
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
