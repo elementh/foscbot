f4781c6f98558994a0356009535d66dd3d341630
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-05-26 20:56:06 +0200

    feat: improve detection of elon musk

--- src/FOSCBot.Core.Domain/Miscellaneous/Elon/ElonMuskMiscellaneousAction.cs
+++ src/FOSCBot.Core.Domain/Miscellaneous/Elon/ElonMuskMiscellaneousAction.cs
@@ -8,7 +8,13 @@ namespace FOSCBot.Core.Domain.Miscellaneous.Elon
     {
         public override bool CanHandle(INavigatorContext ctx)
         {
-            return ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("elon musk") ?? false;
+            return (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("elon musk") ?? false) ||
+                   (ctx.GetMessageOrDefault()?.Text?.ToLower().StartsWith("elon") ?? false) ||
+                   (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("elon ") ?? false) ||
+                   (ctx.GetMessageOrDefault()?.Text?.ToLower().EndsWith(" elon") ?? false) ||
+                   (ctx.GetMessageOrDefault()?.Text?.ToLower().StartsWith("musk") ?? false) ||
+                   (ctx.GetMessageOrDefault()?.Text?.ToLower().Contains("musk ") ?? false) ||
+                   (ctx.GetMessageOrDefault()?.Text?.ToLower().EndsWith(" musk") ?? false);
         }
     }
 }
