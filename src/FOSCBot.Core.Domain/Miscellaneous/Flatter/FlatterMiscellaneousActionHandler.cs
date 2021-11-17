dcd9379773b012a521fe95434a95452b8858f72b
linuxct <linuxct@linuxct.space>
2021-11-17 22:53:49 +0100

    feat: Quote replies on Flatter actions

--- src/FOSCBot.Core.Domain/Miscellaneous/Flatter/FlatterMiscellaneousActionHandler.cs
+++ src/FOSCBot.Core.Domain/Miscellaneous/Flatter/FlatterMiscellaneousActionHandler.cs
@@ -21,27 +21,27 @@ namespace FOSCBot.Core.Domain.Miscellaneous.Flatter
             switch (choice)
             {
                 case 0:
-                    await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), "De nada hermozo 😘", cancellationToken: cancellationToken);
+                    await Ctx.Client.SendTextMessageAsync(Ctx.GetTelegramChat(), "De nada hermozo 😘", cancellationToken: cancellationToken, replyToMessageId: request.MessageId);
                     break;
                 case 1:
                     // Smiling rani 
-                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgIAAxkBAAEDJMNhdZKneWmWSMJ-5BOOyTK5y4dRpgACCgEAAjDUnRFWVFdpxm65byEE", cancellationToken: cancellationToken);
+                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgIAAxkBAAEDJMNhdZKneWmWSMJ-5BOOyTK5y4dRpgACCgEAAjDUnRFWVFdpxm65byEE", cancellationToken: cancellationToken, replyToMessageId: request.MessageId);
                     break;
                 case 2:
                     // Moon smiling broken 
-                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgIAAxkBAAEDJMlhdZQmchXArRkMCRchHWpgPNLZfgACQQoAAiqWeEhXs1wuuE0lniEE", cancellationToken: cancellationToken);
+                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgIAAxkBAAEDJMlhdZQmchXArRkMCRchHWpgPNLZfgACQQoAAiqWeEhXs1wuuE0lniEE", cancellationToken: cancellationToken, replyToMessageId: request.MessageId);
                     break;
                 case 3:
                     // Me aburris tio
-                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgQAAxkBAAEDJMthdZQwLAIyUcECwynw-TuPe_87fAACUgMAApjnowABWVTvcB6NosQhBA", cancellationToken: cancellationToken);
+                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgQAAxkBAAEDJMthdZQwLAIyUcECwynw-TuPe_87fAACUgMAApjnowABWVTvcB6NosQhBA", cancellationToken: cancellationToken, replyToMessageId: request.MessageId);
                     break;
                 case 4:
                     // P4 Arch broken
-                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgQAAxkBAAEDJM1hdZU5WpnzPHDOqI1SLIc5oZuz9gACWwIAApDUrQYyy_1Go-xzYiEE", cancellationToken: cancellationToken);
+                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgQAAxkBAAEDJM1hdZU5WpnzPHDOqI1SLIc5oZuz9gACWwIAApDUrQYyy_1Go-xzYiEE", cancellationToken: cancellationToken, replyToMessageId: request.MessageId);
                     break;
                 case 5:
                     // Croco nice
-                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgIAAxkBAAEDJNFhdZYD0vurwr7VikMz-SbM0TDhSgACLgkAAhhC7ghmx6Iwr7yx9CEE", cancellationToken: cancellationToken);
+                    await Ctx.Client.SendStickerAsync(Ctx.GetTelegramChat(), "CAACAgIAAxkBAAEDJNFhdZYD0vurwr7VikMz-SbM0TDhSgACLgkAAhhC7ghmx6Iwr7yx9CEE", cancellationToken: cancellationToken, replyToMessageId: request.MessageId);
                     break;
             }
             
