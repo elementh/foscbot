acf5c6e9b1ee558d3c44a447aea607e887fa22d8
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-04-06 12:22:44 +0200

    fix: correct MentionHelper to match latest Navigator

--- src/FOSCBot.Common/Helper/MentionHelper.cs
+++ src/FOSCBot.Common/Helper/MentionHelper.cs
@@ -1,5 +1,7 @@
 using System.Text.RegularExpressions;
-using Navigator.Abstractions;
+using Navigator.Context;
+using Navigator.Context.Extensions.Bundled.OriginalEvent;
+using Telegram.Bot.Types;
 
 namespace FOSCBot.Common.Helper;
 
@@ -27,9 +29,9 @@ public static class MentionHelper
     /// <returns>true if the bot was mentioned</returns>
     public static bool IsBotMentioned(this INavigatorContext ctx)
     {
-        return (ctx.Update.Message?.Text?.ToLower().Contains("@foscbot") ?? false) ||
-               (ctx.Update.Message?.Text?.ToLower().Contains("foscbot") ?? false) ||
-               (ctx.Update.Message?.Text?.ToLower().Contains("fosbo") ?? false);
+        return (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("@foscbot") ?? false) ||
+               (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("foscbot") ?? false) ||
+               (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("fosbo") ?? false);
     }
         
     /// <summary>
@@ -39,7 +41,7 @@ public static class MentionHelper
     /// <returns>true if the bot was mentioned</returns>
     public static bool IsBotQuoted(this INavigatorContext ctx)
     {
-        return ctx.Update.Message?.ReplyToMessage?.From?.Id == FoscBotUserId;
+        return ctx.GetOriginalEventOrDefault<Update>()?.Message?.ReplyToMessage?.From?.Id == FoscBotUserId;
     }
         
     /// <summary>
@@ -50,7 +52,7 @@ public static class MentionHelper
     /// <returns>true if the bot was pinged</returns>
     public static bool IsBotPinged(this INavigatorContext ctx)
     {
-        return Regex.IsMatch(ctx.Update.Message?.Text ?? string.Empty, ".*[Ee]*[Ss]*[Tt][AaÁá][Ss] [BbVv][Ii]+[EeVv]+[NnOo]+.*");
+        return Regex.IsMatch(ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text ?? string.Empty, ".*[Ee]*[Ss]*[Tt][AaÁá][Ss] [BbVv][Ii]+[EeVv]+[NnOo]+.*");
     }
 
     /// <summary>
@@ -60,17 +62,17 @@ public static class MentionHelper
     /// <returns>true if the bot was flattered</returns>
     public static bool IsBotFlattered(this INavigatorContext ctx)
     {
-        return ((ctx.Update.Message?.Text?.ToLower().Equals("si") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Equals("sí") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("acho") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("jajaja") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("gracias") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("te quiero") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("grande") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("increible") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("increíble") ?? false) ||
-                (ctx.Update.Message?.Text?.ToLower().Contains("puto amo") ?? false) ||
-                Regex.IsMatch(ctx.Update.Message?.Text ?? string.Empty, @"[Jj][Oo]+[Dd][Ee]+[Rr]+"));
+        return ((ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Equals("si") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Equals("sí") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("acho") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("jajaja") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("gracias") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("te quiero") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("grande") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("increible") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("increíble") ?? false) ||
+                (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("puto amo") ?? false) ||
+                Regex.IsMatch(ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text ?? string.Empty, @"[Jj][Oo]+[Dd][Ee]+[Rr]+"));
     }
 
     /// <summary>
@@ -80,11 +82,11 @@ public static class MentionHelper
     /// <returns>true if the bot was told a bad thing</returns>
     public static bool IsBotBeingToldBadThings(this INavigatorContext ctx)
     {
-        return (ctx.Update.Message?.Text?.ToLower().Contains("bad bot") ?? false) ||
-               (ctx.Update.Message?.Text?.ToLower().Contains("bot malo") ?? false) ||
-               (ctx.Update.Message?.Text?.ToLower().Contains("mal bot") ?? false) ||
-               (ctx.Update.Message?.Text?.ToLower().Contains("basta") ?? false) ||
-               (ctx.Update.Message?.Text?.ToLower().Contains("para") ?? false) ||
-               (ctx.Update.Message?.Text?.ToLower().Contains("capullo") ?? false);
+        return (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("bad bot") ?? false) ||
+               (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("bot malo") ?? false) ||
+               (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("mal bot") ?? false) ||
+               (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("basta") ?? false) ||
+               (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("para") ?? false) ||
+               (ctx.GetOriginalEventOrDefault<Update>()?.Message?.Text?.ToLower().Contains("capullo") ?? false);
     }
 }
