18235dc930ffc82958be21e51d1589ee307700b2
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-04-07 12:18:45 +0200

    feat: implement generic handler for interop actions

---
+++ src/FOSCBot.Core.Domain/Interop/Handler/FOSCInteropActionHandler.cs
@@ -0,0 +1,23 @@
+using Navigator.Context;
+using Navigator.Extensions.Interop;
+using Navigator.Providers.Telegram;
+using Telegram.Bot.Types;
+
+namespace FOSCBot.Core.Domain.Interop;
+
+public class FOSCInteropActionHandler : InteropActionHandler
+{
+    public FOSCInteropActionHandler(INavigatorContextAccessor navigatorContextAccessor) : base(navigatorContextAccessor)
+    {
+    }
+
+    protected override Task<object> ArgsBuilder()
+    {
+        return Task.FromResult<object>(new
+        {
+            context = NavigatorContext,
+            client = NavigatorContext.GetTelegramClient(),
+            chat = new ChatId(NavigatorContext.GetTelegramChat().Id),
+        });
+    }
+}
