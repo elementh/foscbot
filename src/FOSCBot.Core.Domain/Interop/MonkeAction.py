547870a69a5616be4d2a38d87b18cd51e5b2080a
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-04-14 16:00:28 +0200

    fix: correct action

--- src/FOSCBot.Core.Domain/Interop/MonkeAction.py
+++ src/FOSCBot.Core.Domain/Interop/MonkeAction.py
@@ -1,7 +1,7 @@
 # This imports the client for sending responses and interacting with telegram
 from Telegram.Bot import TelegramBotClientExtensions as Navigator
 from Telegram.Bot.Types.InputFiles import InputOnlineFile
-import System
+
 
 # Needed to understand if this module can handle the current message event
 def can_handle_current_context(context, event):
@@ -19,5 +19,5 @@ def can_handle_current_context(context, event):
 def handle(conversation, event, args):
     # To send a sticker or any kind of image or gif, we need to construct an InputOnlineFile object.
     # It accepts media ids (stickers, gifs, previous images, etc.) and also accepts urls for images and gifs.
-    photo = InputOnlineFile("https://raw.githubusercontent.com/elementh/foscbot/main/assets/monke.gif")
-    Navigator.SendPhotoAsync(args.client, args.chat, photo, caption="RETURN TO MONKE FTW")
+    photo = InputOnlineFile("https://raw.githubusercontent.com/elementh/foscbot/main/assets/monke.mp4")
+    Navigator.SendVideoAsync(args.client, args.chat, photo, captcion="RETURN TO MONKE FTW")
