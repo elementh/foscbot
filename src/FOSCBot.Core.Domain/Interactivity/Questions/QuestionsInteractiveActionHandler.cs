15dc5a3094b93df0c51123b3e3f77806a71b356b
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-08-21 17:51:27 +0300

    fix: correct trigger

--- src/FOSCBot.Core.Domain/Interactivity/Questions/QuestionsInteractiveActionHandler.cs
+++ src/FOSCBot.Core.Domain/Interactivity/Questions/QuestionsInteractiveActionHandler.cs
@@ -13,7 +13,8 @@ public class QuestionsInteractiveActionHandler : ActionHandler<QuestionsInteract
     private readonly IDistributedCache _distributedCache;
     private readonly ILlamaService _llamaService;
 
-    public QuestionsInteractiveActionHandler(INavigatorContextAccessor navigatorContextAccessor, IDistributedCache distributedCache, ILlamaService llamaService) : base(navigatorContextAccessor)
+    public QuestionsInteractiveActionHandler(INavigatorContextAccessor navigatorContextAccessor, IDistributedCache distributedCache,
+        ILlamaService llamaService) : base(navigatorContextAccessor)
     {
         _distributedCache = distributedCache;
         _llamaService = llamaService;
@@ -28,35 +29,41 @@ public class QuestionsInteractiveActionHandler : ActionHandler<QuestionsInteract
 
         var odds = RandomProvider.GetThreadRandom().Next(0, 20);
 
-        if (odds > 10)
-        {
-            response = await _llamaService.GetResponse(new[] { action.Message.Text! }, default) ?? QuestionsInteractiveResponseData.ChillResponses.GetRandomFromList();
-        }
-        
         if (int.TryParse(cacheValue, out var questionsAsked))
         {
-            response = questionsAsked switch
+            if (odds > 10)
             {
-                > 10 => QuestionsInteractiveResponseData.OutOfMindResponses.GetRandomFromList(),
-                > 3 => QuestionsInteractiveResponseData.DramaticResponses.GetRandomFromList(),
-                _ => QuestionsInteractiveResponseData.ChillResponses.GetRandomFromList()
-            };
+                response = await _llamaService.GetResponse(new[] { action.Message.Text! }, default) ??
+                           QuestionsInteractiveResponseData.ChillResponses.GetRandomFromList();
+            }
+            else
+            {
+                response = questionsAsked switch
+                {
+                    > 10 => QuestionsInteractiveResponseData.OutOfMindResponses.GetRandomFromList(),
+                    > 3 => QuestionsInteractiveResponseData.DramaticResponses.GetRandomFromList(),
+                    _ => QuestionsInteractiveResponseData.ChillResponses.GetRandomFromList()
+                };
+            }
         }
         else
         {
-            response = await _llamaService.GetResponse(new[] { action.Message.Text! }, default) ?? QuestionsInteractiveResponseData.ChillResponses.GetRandomFromList();
+            response = await _llamaService.GetResponse(new[] { action.Message.Text! }, default) ??
+                       QuestionsInteractiveResponseData.ChillResponses.GetRandomFromList();
         }
 
         await _distributedCache.SetStringAsync(cacheKey, $"{questionsAsked + 1}",
             new DistributedCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(30)), cancellationToken);
-        
+
         if (response.IsSticker())
         {
-            await NavigatorContext.GetTelegramClient().SendStickerAsync(NavigatorContext.GetTelegramChat()!, response, cancellationToken: cancellationToken);
+            await NavigatorContext.GetTelegramClient()
+                .SendStickerAsync(NavigatorContext.GetTelegramChat()!, response, cancellationToken: cancellationToken);
         }
         else
         {
-            await NavigatorContext.GetTelegramClient().SendTextMessageAsync(NavigatorContext.GetTelegramChat()!, response, cancellationToken: cancellationToken);
+            await NavigatorContext.GetTelegramClient()
+                .SendTextMessageAsync(NavigatorContext.GetTelegramChat()!, response, cancellationToken: cancellationToken);
         }
 
         return Success();
