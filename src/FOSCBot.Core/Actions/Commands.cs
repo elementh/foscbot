6ac9f43592a3a27839f7c3ec458d28f6a4fdfc79
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-21 18:36:13 +0200

    feat: reimplement bottomify command

--- src/FOSCBot.Core/Actions/Commands.cs
+++ src/FOSCBot.Core/Actions/Commands.cs
@@ -1,10 +1,10 @@
-using FOSCBot.Core.Resources;
+using Bottom;
 using Incremental.Common.Random;
 using Navigator.Catalog.Factory;
 using Navigator.Catalog.Factory.Extensions;
 using Navigator.Client;
-using Navigator.Entities;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace FOSCBot.Core.Actions;
@@ -37,6 +37,35 @@ public static class Commands
             var randomSticker = stickers[RandomProvider.GetThreadRandom()!.Next(0, stickers.Length)];
 
             await client.SendStickerAsync(chat, randomSticker);
+        });
+        
+        catalog.OnCommand("bottomify", async (INavigatorClient client, Chat chat, Message message) =>
+        {
+            string[] lines =
+            [
+                "Idiot human is idiot.",
+                "For the stolen code!",
+                "P4 was once an Apple fanboy.",
+                "I'm in this picture and I don't like it.",
+                "I dislike proprietary code as much as DeepCreamPy."
+            ];
+            
+            if (string.IsNullOrWhiteSpace(message.ReplyToMessage?.Text) is false)
+            {
+                return await client.SendTextMessageAsync(chat, Bottomify.EncodeString(message.ReplyToMessage.Text),
+                    replyParameters: message.ReplyToMessage);
+            }
+            
+            var input = message.Text?.Remove(0, message.Text.IndexOf(' ') + 1);
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                return await client.SendTextMessageAsync(chat, Bottomify.EncodeString(input));
+            }
+
+            var randomText = Bottomify.EncodeString(lines[RandomProvider.GetThreadRandom()!.Next(0, lines.Length)]);
+            
+            return await client.SendTextMessageAsync(chat, randomText);
         });
     }
 }
