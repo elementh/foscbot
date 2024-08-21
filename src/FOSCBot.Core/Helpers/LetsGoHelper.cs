2a7acebe6d8fccb5178ab421f3782b9291951a48
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-21 21:33:21 +0200

    refactor: remove Common project and all references to Common from other projects

---
+++ src/FOSCBot.Core/Helpers/LetsGoHelper.cs
@@ -0,0 +1,18 @@
+// namespace FOSCBot.Common.Helper;
+//
+// public static class LetsGoHelper
+// {
+//     public static readonly string[] LetsGoStickers =
+//     {
+//         "CAACAgQAAxkBAAECojpg_bnnG-u3pLeJOG5IPolOtN00RAACBQwAApUS8FOOn4aL83n8bSAE",
+//         "CAACAgQAAxkBAAECojxg_bnqH-1SeiLQLt_KypOZBI5uowACmQ8AAmFh8FO6uZ5fZWppOiAE",
+//         "CAACAgQAAxkBAAECoj5g_bp_i3lF_F8dZQK8ZLEvKK3eJgAC3gkAAvPw8VPNHKHR3O21xiAE",
+//         "CAACAgQAAxkBAAECokBg_bqCvo4wxPdsQfKZK4WqdCky0wACvQgAAs326VOcqEIdovpiBCAE",
+//         "CAACAgQAAxkBAAECokJg_bqH1nmA0VD9OwRaaJ7XzcbRngAC6AgAAjFY8FOa7ph-XJo5GSAE",
+//         "CAACAgQAAxkBAAECokRg_bqfolorzkJYdp9yC4DKHmiO9gACEgsAAum88VObzYkkVjOSUyAE",
+//         "CAACAgQAAxkBAAECokZg_bqj-FylWbafjgm9f_h1oXrahQAC7QoAAtmJ8FNTWKR0OIY5USAE",
+//         "CAACAgQAAxkBAAECokhg_brMFoDgdE2fx2KYV2YYVR6s9wACggsAAm5s8VOsGad1nfXzfCAE",
+//         "CAACAgQAAxkBAAECokpg_brRLEoWV1mx8Sxlgvwg7XU-WAACLQwAAlWt8VN-tLCZkqsk_yAE",
+//         "CAACAgQAAxkBAAECokxg_brVzW5P93IB4ObTMKKxTzSKYAAC6QkAAlWw8FN6EYT5JghtsyAE"
+//     };
+// }
