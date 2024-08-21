c5b54a8c19af72e348a969c34798f3ffa1056740
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-21 18:10:31 +0200

    feat: reimplement boi command

--- src/FOSCBot.Core/Actions/Commands.cs
+++ src/FOSCBot.Core/Actions/Commands.cs
@@ -15,7 +15,6 @@ public static class Commands
     {
         catalog.OnCommand("about", async (INavigatorClient client, Chat chat) =>
         {
-            await client.SendTextMessageAsync(chat, CoreResources.AboutText, parseMode:ParseMode.Markdown);
             const string about = """
                                  More info about FOSC at [fosc.space](https://fosc.space).
                                  Check out my source code at [foscbot](https://github.com/elementh/foscbot).
@@ -24,6 +23,20 @@ public static class Commands
                                  """;
             
             await client.SendTextMessageAsync(chat, about, parseMode: ParseMode.Markdown);
+        });
+
+        catalog.OnCommand("boi", async (INavigatorClient client, Chat chat) =>
+        {
+            string[] stickers =
+            [
+                "CAACAgQAAxkBAAI5Hl59wgU-EjcPoQJXOk81dz15rU6vAAIXAAMN0oYC8U1SU9k9QFQYBA",
+                "CAACAgQAAxkBAAI5H159wgZdNShe0hTs65NLdinvP9v5AAIOAAMN0oYCfWiupsMYNyUYBA",
+                "CAACAgQAAxkBAAI5Il59wgpjoo8hIuSmS7HRs44rneMRAAIeAAMN0oYCa2VzcOI0p0sYBA"
+            ];
+
+            var randomSticker = stickers[RandomProvider.GetThreadRandom()!.Next(0, stickers.Length)];
+
+            await client.SendStickerAsync(chat, randomSticker);
         });
     }
 }
