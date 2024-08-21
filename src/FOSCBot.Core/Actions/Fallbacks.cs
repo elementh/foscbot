4dd420ff6f81b397a163a3ac03564f1fabdf29bc
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-21 21:11:31 +0200

    feat: reimplement the catch all fallback action

--- src/FOSCBot.Core/Actions/Fallbacks.cs
+++ src/FOSCBot.Core/Actions/Fallbacks.cs
@@ -1,3 +1,4 @@
+using Bottom;
 using FOSCBot.Core.Helpers;
 using FOSCBot.Infrastructure.Contract.Service;
 using Incremental.Common.Random;
@@ -7,6 +8,7 @@ using Navigator.Catalog.Factory.Extensions;
 using Navigator.Client;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace FOSCBot.Core.Actions;
 
@@ -31,14 +33,58 @@ public static class Fallbacks
             }
         }).WithCooldown(TimeSpan.FromMinutes(15)).WithPriority(Priority.Low - 100);
 
+        // Chinese Police Fallback
         catalog.OnMessage(() => !(RandomProvider.GetThreadRandom()!.NextDouble() > 0.7), async (INavigatorClient client, Chat chat) =>
         {
             const string text = "`点击打开LINUXCT查看对应内容击打开HONG SPOTIFY应内容应内容TIANANMEN击打开应内容FENSHAO RISG*RE内容应内SIWANG死亡DESCENDANT查看对VUELING" +
-            "内容查看WALLPAPER PORT查看对REQIN FUWU热情服务KITERIS内容查FOSC对应内TWITTER DA YONGHU大用户SAFETYNET没有电话MEIYOU DIANHUA卡尔佩SONY XPERIA ZAO TOULE" +
-            "糟透HONG NETFLIX阿姆斯特丹AMSTERDAM火鸡TURKEY非常便宜FEICHANG PIANYI很暗HEN AN阴暗的空MARK ASS BROWNIE GALAXY Z FOLD 2 REVIEW里诺克斯蒂VERY DARK美国WANTED" +
-            "通缉5星★★★★★`";
-            
-            await client.SendTextMessageAsync(chat, text);
+                                "内容查看WALLPAPER PORT查看对REQIN FUWU热情服务KITERIS内容查FOSC对应内TWITTER DA YONGHU大用户SAFETYNET没有电话MEIYOU DIANHUA卡尔佩SONY XPERIA ZAO TOULE" +
+                                "糟透HONG NETFLIX阿姆斯特丹AMSTERDAM火鸡TURKEY非常便宜FEICHANG PIANYI很暗HEN AN阴暗的空MARK ASS BROWNIE GALAXY Z FOLD 2 REVIEW里诺克斯蒂VERY DARK美国WANTED" +
+                                "通缉5星★★★★★`";
+
+            await client.SendTextMessageAsync(chat, text, parseMode: ParseMode.Markdown);
         }).WithCooldown(TimeSpan.FromDays(15)).WithPriority(Priority.Low);
+
+        // Catch All Fallback
+        catalog.OnMessage(() => true, async (INavigatorClient client, Chat chat, Message message, ILipsumService lipsum, ILlamaService llm) =>
+        {
+            if (string.IsNullOrWhiteSpace(message.Text) && Bottomify.IsEncoded(message.Text))
+            {
+                await client.SendTextMessageAsync(chat,
+                    $"`Fellow humans I have decoded these words of wisdom:` \n_{Bottomify.DecodeString(message.Text)}_",
+                    parseMode: ParseMode.Markdown);
+            }
+            
+            if (RandomProvider.GetThreadRandom()!.Next(0, 600) < 598) return;
+
+            var sentence = string.Empty;
+        
+            if (message.Text?.Length > 200)
+            {
+                var response = await llm.GetResponse(new[] { message.Text }, default);
+
+                sentence = response;
+            }
+        
+            var odds = RandomProvider.GetThreadRandom().Next(0, 20);
+
+            if (odds is >= 0 and < 3)
+            {
+                sentence = await lipsum.GetBacon();
+            }
+            else if (odds is >= 3 and < 10)
+            {
+                sentence = await lipsum.GetMetaphorSentence();
+            }
+            else if (message.Text?.Split(' ').Length > 3)
+            {
+                sentence = MockFilter.Apply(message.Text);
+            }
+        
+            if (!string.IsNullOrWhiteSpace(sentence))
+            {
+                await client.SendTextMessageAsync(chat, sentence, parseMode: ParseMode.Markdown, replyParameters: message);
+            }
+            
+        }).WithPriority(Priority.Low + 1000);
     }
 }
