0343f2294a614d45cd8e8a69feab7c327110b2a5
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-22 11:34:48 +0200

    feat: reimplement jeje action

--- src/FOSCBot.Core/Actions/Stickers.cs
+++ src/FOSCBot.Core/Actions/Stickers.cs
@@ -124,5 +124,25 @@ public static class Stickers
                 "CAACAgIAAxkBAAECGY1gWyj6c-QMcGzNPWtfiGaPZE0WcwACkggAAnlc4glg2uUwtwJdvR4E"
             ])
             .WithName("Felon Mask");
+
+        catalog
+            .OnText((string text) =>
+                text.ToLower().Contains("je ") ||
+                text.ToLower().Contains(" je") ||
+                text.ToLower().Contains(" je ") ||
+                text.ToLower().Contains("jeje") ||
+                text.ToLower().Equals("je") ||
+                text.ToLower().Contains("je je")
+            )
+            .SendRandomStickerFrom([
+                "CAACAgIAAxkBAAI5Dl59vPOH6MA6Uzua49AHRz-q5mMUAAIKAQACMNSdEVZUV2nGbrlvGAQ",
+                "CAACAgQAAxkBAAI5EF59vUKLQ46GEgbuzhY0O5r3HyauAAJKAQACqCEhBntEKK5RNh4XGAQ",
+                "CAACAgIAAxkBAAI5El59vYqk6ywiJOKdXzXNe2gsPL2gAAL2AwACierlB263K9ogJ_bwGAQ",
+                "CAACAgIAAxkBAAI5FF59vZFmQWYDsTaj4X9GJl9bPAbEAAJBAQAC-YQfHIsWbGjJcnqnGAQ",
+                "CAACAgQAAxkBAAI5Fl59vb9993hlyxnbQ_VEZlEMqzymAAI7AgACMo1bAAEaE0PNwutkzBgE",
+                "CAACAgIAAxkBAAI5GF59vfIG0bug-aIj8txxEBNNiNUXAAIQCAACGELuCOAfnJHe30ZuGAQ",
+                "CAACAgIAAxkBAAI5Gl59vfg4AefyKXIXUAMOdoCs6gNAAALNBwACGELuCPlfWYiQZaQiGAQ"
+            ])
+            .WithName("Jeje");
     }
 }
