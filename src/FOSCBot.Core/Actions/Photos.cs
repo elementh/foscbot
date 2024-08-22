42374158f16da65a7c95f16402dab976be36ae26
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-22 11:51:19 +0200

    feat: reimplement HeyBro action

---
+++ src/FOSCBot.Core/Actions/Photos.cs
@@ -0,0 +1,29 @@
+using FOSCBot.Core.Resources;
+using Incremental.Common.Random;
+using Navigator.Catalog.Factory;
+using Navigator.Catalog.Factory.Extensions;
+using Navigator.Client;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FOSCBot.Core.Actions;
+
+public static class Photos
+{
+    public static void RegisterPhotos(this BotActionCatalogFactory catalog)
+    {
+        catalog.OnSticker((Sticker sticker) =>
+        {
+            if (RandomProvider.GetThreadRandom()!.NextDouble() > 0.5d) return false;
+
+            return sticker.SetName?.Equals("foscupct") is not false && sticker.Emoji?.Equals("😚") is true;
+        }, async (INavigatorClient client, Chat chat) =>
+        {
+            var bytes = Convert.FromBase64String(CoreResources.HeyBroImage);
+            
+            await using var stream = await new StreamContent(new MemoryStream(bytes)).ReadAsStreamAsync();
+
+            await client.SendPhotoAsync(chat, new InputFileStream(stream, "heybroniced.jpg"));
+        });
+    }
+}
