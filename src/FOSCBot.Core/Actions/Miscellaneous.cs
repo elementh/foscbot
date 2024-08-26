ac63f8b19d059bca58442e8bc87414be5011ece3
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-26 13:17:25 +0200

    feat: reimplement both LETS actions

--- src/FOSCBot.Core/Actions/Miscellaneous.cs
+++ src/FOSCBot.Core/Actions/Miscellaneous.cs
@@ -255,6 +255,57 @@ public static partial class Miscellaneous
             .WithName("Miscellaneous.Lantra");
 
         catalog
+            .OnText((string text) => text.Equals("LETS") || text.Equals("LET'S"))
+            .SetHandler(async (INavigatorClient client, Chat chat) =>
+            {
+                string[] stickerList = [
+                    "CAACAgQAAxkBAAECojpg_bnnG-u3pLeJOG5IPolOtN00RAACBQwAApUS8FOOn4aL83n8bSAE",
+                    "CAACAgQAAxkBAAECojxg_bnqH-1SeiLQLt_KypOZBI5uowACmQ8AAmFh8FO6uZ5fZWppOiAE",
+                    "CAACAgQAAxkBAAECoj5g_bp_i3lF_F8dZQK8ZLEvKK3eJgAC3gkAAvPw8VPNHKHR3O21xiAE",
+                    "CAACAgQAAxkBAAECokBg_bqCvo4wxPdsQfKZK4WqdCky0wACvQgAAs326VOcqEIdovpiBCAE",
+                    "CAACAgQAAxkBAAECokJg_bqH1nmA0VD9OwRaaJ7XzcbRngAC6AgAAjFY8FOa7ph-XJo5GSAE",
+                    "CAACAgQAAxkBAAECokRg_bqfolorzkJYdp9yC4DKHmiO9gACEgsAAum88VObzYkkVjOSUyAE",
+                    "CAACAgQAAxkBAAECokZg_bqj-FylWbafjgm9f_h1oXrahQAC7QoAAtmJ8FNTWKR0OIY5USAE",
+                    "CAACAgQAAxkBAAECokhg_brMFoDgdE2fx2KYV2YYVR6s9wACggsAAm5s8VOsGad1nfXzfCAE",
+                    "CAACAgQAAxkBAAECokpg_brRLEoWV1mx8Sxlgvwg7XU-WAACLQwAAlWt8VN-tLCZkqsk_yAE",
+                    "CAACAgQAAxkBAAECokxg_brVzW5P93IB4ObTMKKxTzSKYAAC6QkAAlWw8FN6EYT5JghtsyAE"
+                ];
+                
+                var randomSticker = stickerList[RandomProvider.GetThreadRandom()!.Next(0, stickerList.Length)];
+
+                await client.SendTextMessageAsync(chat, "FUCKING");
+
+                await client.SendChatActionAsync(chat, ChatAction.Typing);
+                await Task.Delay(200);
+                await client.SendTextMessageAsync(chat, "GO");
+
+                await client.SendChatActionAsync(chat, ChatAction.ChooseSticker);
+                await Task.Delay(200);
+                await client.SendStickerAsync(chat, randomSticker);
+            })
+            .WithName("Miscellaneous.Lets");
+
+        catalog
+            .OnText((string text) => text.ToLower().StartsWith("let's fucking go")
+                                     || text.ToLower().StartsWith("lets fucking go")
+                                     || text.ToLower().StartsWith("let's fuckin go")
+                                     || text.ToLower().StartsWith("lets fuckin go"))
+            .SendRandomStickerFrom([
+                "CAACAgQAAxkBAAECojpg_bnnG-u3pLeJOG5IPolOtN00RAACBQwAApUS8FOOn4aL83n8bSAE",
+                "CAACAgQAAxkBAAECojxg_bnqH-1SeiLQLt_KypOZBI5uowACmQ8AAmFh8FO6uZ5fZWppOiAE",
+                "CAACAgQAAxkBAAECoj5g_bp_i3lF_F8dZQK8ZLEvKK3eJgAC3gkAAvPw8VPNHKHR3O21xiAE",
+                "CAACAgQAAxkBAAECokBg_bqCvo4wxPdsQfKZK4WqdCky0wACvQgAAs326VOcqEIdovpiBCAE",
+                "CAACAgQAAxkBAAECokJg_bqH1nmA0VD9OwRaaJ7XzcbRngAC6AgAAjFY8FOa7ph-XJo5GSAE",
+                "CAACAgQAAxkBAAECokRg_bqfolorzkJYdp9yC4DKHmiO9gACEgsAAum88VObzYkkVjOSUyAE",
+                "CAACAgQAAxkBAAECokZg_bqj-FylWbafjgm9f_h1oXrahQAC7QoAAtmJ8FNTWKR0OIY5USAE",
+                "CAACAgQAAxkBAAECokhg_brMFoDgdE2fx2KYV2YYVR6s9wACggsAAm5s8VOsGad1nfXzfCAE",
+                "CAACAgQAAxkBAAECokpg_brRLEoWV1mx8Sxlgvwg7XU-WAACLQwAAlWt8VN-tLCZkqsk_yAE",
+                "CAACAgQAAxkBAAECokxg_brVzW5P93IB4ObTMKKxTzSKYAAC6QkAAlWw8FN6EYT5JghtsyAE"
+            ])
+            .WithChatAction(ChatAction.ChooseSticker)
+            .WithName("Miscellaneous.LetsGo");
+
+        catalog
             .OnText((string text) =>
                 text.ToLower().Contains("so sad") || text.ToLower().Contains("ligma") || text.ToLower().Contains("p4cock"))
             .SetHandler(async (INavigatorClient client, Chat chat) =>
@@ -438,12 +489,14 @@ public static partial class Miscellaneous
                     case 2:
                         await client.SendChatActionAsync(chat, ChatAction.UploadVideo);
                         await Task.Delay(200);
-                        await client.SendVideoAsync(chat, "https://raw.githubusercontent.com/elementh/foscbot/master/assets/megatron_cbt_experience.mp4");
+                        await client.SendVideoAsync(chat,
+                            "https://raw.githubusercontent.com/elementh/foscbot/master/assets/megatron_cbt_experience.mp4");
                         break;
                     case 3:
                         await client.SendChatActionAsync(chat, ChatAction.UploadVideo);
                         await Task.Delay(200);
-                        await client.SendVideoAsync(chat, "https://raw.githubusercontent.com/elementh/foscbot/master/assets/megatron_cbt_immediate.mp4");
+                        await client.SendVideoAsync(chat,
+                            "https://raw.githubusercontent.com/elementh/foscbot/master/assets/megatron_cbt_immediate.mp4");
                         break;
                 }
             })
