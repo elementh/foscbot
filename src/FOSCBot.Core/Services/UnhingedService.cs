267fb590b9e87e5d3b3d4f5d64316fe75d42bbcf
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-15 17:57:49 +0200

    feat: implement UnhingedService

---
+++ src/FOSCBot.Core/Services/UnhingedService.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Caching.Memory;
+
+namespace FOSCBot.Core.Services;
+
+public class UnhingedService(IMemoryCache cache)
+{
+    public bool IsUnhinged(long chatId)
+    {
+        return cache.Get<bool>($"unhinged:{chatId}");
+    }
+
+    public void SetUnhinged(long chatId)
+    {
+        cache.Set($"unhinged:{chatId}", true);
+    }
+
+    public void Clear(long chatId)
+    {
+        cache.Remove($"unhinged:{chatId}");
+    }
+}
